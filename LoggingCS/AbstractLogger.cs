9ae9ebfd683d5c22201311924a959c9202186896
Shreyaas14 <shreyaas.sureshbabu114@gmail.com>
2024-02-29 22:28:31 -0500

    implemented abstract logger method

---
+++ LoggingCS/AbstractLogger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradingEngineServer.Logging
+{
+    abstract class AbstractLogger : ILogger
+    {
+        public void Debug(string module, string message) => Log(LogLevel.Debug, module, message);
+
+        public void Debug(string module, Exception exception) => Log(LogLevel.Debug, module, exception.ToString());
+
+        public void Information(string module, string message) => Log(LogLevel.Information, module, message);
+
+        public void Information(string module, Exception exception) => Log(LogLevel.Information, module, exception.ToString());
+
+        public void Warning(string module, string message) => Log(LogLevel.Warning, module, message);
+
+        public void Warning(string module, Exception exception) => Log(LogLevel.Warning, module, exception.ToString());
+
+        public void Error(string module, string message) => Log(LogLevel.Error, module, message);
+
+        public void Error(string module, Exception exception) => Log(LogLevel.Error, module, exception.ToString());
+
+        protected abstract void Log(LogLevel LogLevel, string module, string message);
+    }
+}
